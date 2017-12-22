698a99f46567af6440d5cc20a0de1a51773d3763
Cristian Stenico <cristian.stenico@praim.com>
2017-12-22 10:29:29 +0100

    aggiunto scss per footer, primi stili

--- components/footer/site-info.php
+++ components/footer/site-info.php
@@ -1,6 +1,17 @@
 <div class="site-info">
-<p>Basket Club Gardolo 2000 ASD</p>
-<p>via Soprasasso, 1 - Gardolo</p>
-<p>38121 TRENTO </p>
-<p>C.F. 96005430226 - P.I. 01140120229</p>
+	<div class="club-info">
+		<p>Basket Club Gardolo 2000 ASD</p>
+		<p>via Soprasasso, 1 - Gardolo</p>
+		<p>38121 TRENTO </p>
+		<p>C.F. 96005430226 - P.I. 01140120229</p>
+	</div>
+	<div class="footer-column">
+		<h5>Squadre</h5>
+	</div>
+	<div class="footer-column">
+		<h5>News</h5>
+	</div>
+	<div class="footer-column">
+		<h5>Lo società</h5>
+	</div>
 </div><!-- .site-info -->
