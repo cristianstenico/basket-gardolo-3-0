73f32afaf6d9c3cc0deac24c418bafaddc454e43
Cristian <cristianstenico@gmail.com>
2018-08-28 23:44:31 +0200

    Fix parentesi

--- components/giornalino/content.php
+++ components/giornalino/content.php
@@ -13,7 +13,7 @@
     <?php
     if (is_single()) {
         the_title( '<h1 class="entry-title">', '</h1>' );
-    } else {}
+    } else {
         the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
     }
     if ('post' === get_post_type()) : ?>
